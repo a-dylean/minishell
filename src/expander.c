058f3a3012f4eb11a34a0161947f071e91bdb92e
Alisa Tonkopiy <atonkopi@bess-f4r6s3.clusters.42paris.fr>
2024-04-16 19:30:40 +0200

    fixed segfault

--- src/expander.c
+++ src/expander.c
@@ -6,7 +6,7 @@
 /*   By: atonkopi <atonkopi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 14:18:21 by atonkopi          #+#    #+#             */
-/*   Updated: 2024/04/16 17:32:55 by atonkopi         ###   ########.fr       */
+/*   Updated: 2024/04/16 19:29:36 by atonkopi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,20 @@ int	expander(t_token *tokens)
 
 int	has_dollar(char *str)
 {
-	int	i;
+    int	i;
 
-	i = 0;
-	while (str[i])
-	{
-		if (str[0] == '$' || (str[i] == '$' && str[i - 1] == ' ')
-			|| (str[i] == '$' && str[i - 1] == '"'))
-			return (1);
-		i++;
-	}
-	return (0);
+    i = 0;
+    if (str == NULL)
+        return (0);
+    while (str[i])
+    {
+        if ((str[i] == '$' && str[i + 1] != ' ' && str[i + 1] != '\0')
+            || (i > 0 && str[i] == '$' && str[i - 1] == ' ') 
+            || (i > 0 && str[i] == '$' && str[i - 1] == '"'))
+            return (1);
+        i++;
+    }
+    return (0);
 }
 
 char	*get_env_from_str(char *str)
@@ -58,10 +61,24 @@ char	*get_env_from_str(char *str)
 	if (!temp)
 		return (NULL);
 	env_var = ft_strtrim(temp, "$\"");
-	free(temp);
 	if (env_var)
 		return (env_var);
+	free(temp);
 	return (NULL);
+}
+
+int	str_is_empty_or_space_only(const char *str)
+{
+	int i;
+	
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 char	*join_strings_with_spaces(char **strings)
@@ -69,15 +86,25 @@ char	*join_strings_with_spaces(char **strings)
 	char	*joined_string;
 	int		i;
 	char	*temp;
-
-	joined_string = ft_strdup(strings[0]);
-	i = 1;
+	
+	joined_string = NULL;
+	i = 0;
+	while (strings[i] && str_is_empty_or_space_only(strings[i]))
+		i++;
+	if (strings[i] == NULL)
+		joined_string = ft_strdup(" ");
+	else
+		joined_string = ft_strdup(strings[i]);
+	i++;
 	while (strings[i])
 	{
-		temp = ft_strjoin(joined_string, " ");
-		free(joined_string);
-		joined_string = ft_strjoin(temp, strings[i]);
-		free(temp);
+		if (!str_is_empty_or_space_only(strings[i]))
+		{
+			temp = ft_strjoin(joined_string, " ");
+			free(joined_string);
+			joined_string = ft_strjoin(temp, strings[i]);
+			free(temp);
+		}
 		i++;
 	}
 	return (joined_string);
@@ -100,7 +127,7 @@ char	*perform_expansion(char *token)
 			env_var = get_env_from_str(split_token[i]);
 			env_value = getenv(env_var);
 			free(split_token[i]);
-			split_token[i] = NULL;
+			split_token[i] = ft_strdup(" ");
 			if (env_value)
 				split_token[i] = ft_strdup(env_value);
 			free(env_var);
