cb37cfccdb710ad3590b680abb010384e2106a04
Julie Labonde <jlabonde@bess-f4r1s8.clusters.42paris.fr>
2024-04-22 11:54:56 +0200

    fixed segfault with \n and cleaned up minishell_loop function

--- src/terminal.c
+++ src/terminal.c
@@ -6,11 +6,30 @@
 /*   By: jlabonde <jlabonde@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 11:43:00 by jlabonde          #+#    #+#             */
-/*   Updated: 2024/04/19 16:26:40 by jlabonde         ###   ########.fr       */
+/*   Updated: 2024/04/22 11:18:15 by jlabonde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+int	trim_and_verify_buffer(char *buffer)
+{
+	buffer = ft_strtrim(buffer, " ");
+	if (!buffer || buffer[0] == '\0')
+	{
+		free(buffer);
+		return (1);
+	}
+	else if (!valid_quotes(buffer))
+	{
+		printf("Error: invalid quotes\n");
+		free(buffer);
+		return (1);
+	}
+	else if (buffer[0] != '\0')
+			add_history(buffer);
+	return (0);
+}
 
 int	minishell_loop()
 {
@@ -20,25 +39,17 @@ int	minishell_loop()
 	buffer = NULL;
 	while ((buffer = readline("minishell$> ")) != NULL)
 	{
-		// handle empty input - example, \n as input
 		tokens = (t_token **)malloc(sizeof(t_token));
 		if (!tokens)
 			return (1);
 		*tokens = NULL;
-		buffer = ft_strtrim(buffer, " ");
-		if (!valid_quotes(buffer))
-		{
-			printf("Error: invalid quotes\n");
-			free_in_terminal(tokens, buffer);
+		if (trim_and_verify_buffer(buffer) == 1)
 			continue ;
-		}
 		if (ft_strncmp(buffer, "exit", 4) == 0)
 		{
 			free_in_terminal(tokens, buffer);
 			break ;
 		}
-		if (buffer[0] != '\0')
-			add_history(buffer);
 		// store the environment in the structure HERE
 		encode_tokens(buffer, tokens);
 		if (!check_syntax(*tokens))
