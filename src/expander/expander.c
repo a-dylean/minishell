7af3062855fbebf97c1dc01dca0f7f71adce1b58
Alisa Tonkopiy <atonkopi@bess-f4r1s3.clusters.42paris.fr>
2024-04-23 18:04:29 +0200

    short quotes check version

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,7 +6,7 @@
 /*   By: atonkopi <atonkopi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 14:18:21 by atonkopi          #+#    #+#             */
-/*   Updated: 2024/04/23 11:24:35 by atonkopi         ###   ########.fr       */
+/*   Updated: 2024/04/23 17:49:51 by atonkopi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,40 +64,114 @@ int	expansion_needed(char *str, int quotes)
 /* function that returns 1 if a string is subject to expansion depending
 on its position in relation to single and double quotes */
 
-int	quotes_check(char *str)
-{
-	int	i;
-	int	in_double_quotes;
-	int	in_single_quotes;
-	int	has_quotes;
+// int	quotes_check(char *str)
+// {
+// 	int	i;
+// 	int	in_double_quotes;
+// 	int	in_single_quotes;
+// 	int	has_quotes;
 
-	i = 0;
-	in_double_quotes = 0;
-	in_single_quotes = 0;
-	has_quotes = 0;
-	while (str[i])
-	{
-		if (str[i] == '\"' && !in_single_quotes)
-		{
-			in_double_quotes = !in_double_quotes;
-			has_quotes = 1;
+// 	i = 0;
+// 	in_double_quotes = 0;
+// 	in_single_quotes = 0;
+// 	has_quotes = 0;
+// 	while (str[i])
+// 	{
+// 		if (str[i] == '\"' && !in_single_quotes)
+// 		{
+// 			in_double_quotes = !in_double_quotes;
+// 			has_quotes = 1;
+// 		}
+// 		else if (str[i] == '\'' && !in_double_quotes)
+// 		{
+// 			in_single_quotes = !in_single_quotes;
+// 			has_quotes = 1;
+// 		}
+// 		else if (str[i] == '$')
+// 		{
+// 			if (in_double_quotes || (in_single_quotes && in_double_quotes))
+// 				return (1);
+// 			else if (in_single_quotes && !in_double_quotes)
+// 				return (0);
+// 		}
+// 		i++;
+// 	}
+// 	return (!has_quotes);
+// }
+
+int quotes_check(char *str) {
+    int inside_single_quotes = 0;
+    int inside_double_quotes = 0;
+
+    while (*str) {
+        if (*str == '\'') {
+            inside_single_quotes = !inside_single_quotes;
+        } else if (*str == '"') {
+            inside_double_quotes = !inside_double_quotes;
+        } else if (*str == '$') {
+            if (!inside_single_quotes)
+                return 1;
 		}
-		else if (str[i] == '\'' && !in_double_quotes)
-		{
-			in_single_quotes = !in_single_quotes;
-			has_quotes = 1;
-		}
-		else if (str[i] == '$')
-		{
-			if (in_double_quotes || (in_single_quotes && in_double_quotes))
-				return (1);
-			else if (in_single_quotes && !in_double_quotes)
-				return (0);
-		}
-		i++;
-	}
-	return (!has_quotes);
+        str++;
+    }
+    return 0;
 }
+
+// int	check_double_quotes(char *str, int *i, int *in_single_quotes, int *in_double_quotes, int *has_quotes)
+// {
+//     if (str[*i] == '\"' && !(*in_single_quotes))
+//     {
+//         *in_double_quotes = !(*in_double_quotes);
+//         *has_quotes = 1;
+//     }
+//     return (*has_quotes);
+// }
+
+// int	check_single_quotes(char *str, int *i, int *in_single_quotes, int *in_double_quotes, int *has_quotes)
+// {
+//     if (str[*i] == '\'' && !(*in_double_quotes))
+//     {
+//         *in_single_quotes = !(*in_single_quotes);
+//         *has_quotes = 1;
+//     }
+//     return (*has_quotes);
+// }
+
+// int	check_dollar_sign(char *str, int *i, int *in_single_quotes, int *in_double_quotes)
+// {
+//     if (str[*i] == '$')
+//     {
+//         if (*in_double_quotes || (*in_single_quotes && *in_double_quotes))
+//             return (1);
+//         else if (*in_single_quotes && !(*in_double_quotes))
+//             return (0);
+//     }
+//     return (-1);
+// }
+
+// int	quotes_check(char *str)
+// {
+//     int	i;
+//     int	in_double_quotes;
+//     int	in_single_quotes;
+//     int	has_quotes;
+//     int	dollar_check;
+
+//     i = 0;
+//     in_double_quotes = 0;
+//     in_single_quotes = 0;
+//     has_quotes = 0;
+//     while (str[i])
+//     {
+//         has_quotes = check_double_quotes(str, &i, &in_single_quotes, &in_double_quotes, &has_quotes);
+//         has_quotes = check_single_quotes(str, &i, &in_single_quotes, &in_double_quotes, &has_quotes);
+//         dollar_check = check_dollar_sign(str, &i, &in_single_quotes, &in_double_quotes);
+//         if (dollar_check != -1)
+//             return (dollar_check);
+//         i++;
+//     }
+//     return (!has_quotes);
+// }
 
 /* function that calculates the size of the expansion
 that will be added to the buffer */
