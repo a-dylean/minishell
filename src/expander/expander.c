cd46430276c9594bfafb555a0cec9228b1d8cc83
Alisa Tonkopiy <atonkopi@bess-f4r6s4.clusters.42paris.fr>
2024-04-24 15:40:00 +0200

    fix quotes func bug

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,7 +6,7 @@
 /*   By: atonkopi <atonkopi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 14:18:21 by atonkopi          #+#    #+#             */
-/*   Updated: 2024/04/24 15:14:26 by atonkopi         ###   ########.fr       */
+/*   Updated: 2024/04/24 15:39:26 by atonkopi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,14 @@ void	set_quotes_status(t_token *tokens)
 	int		in_double_quotes;
 
 	temp = tokens;
-	in_single_quotes = 0;
-	in_double_quotes = 0;
+
 	while (temp)
 	{
-		if (temp->value[0] == S_QUOTE && temp->quotes_status != DQUOTED)
+		in_single_quotes = 0;
+		in_double_quotes = 0;
+		if (temp->value[0] == S_QUOTE && !in_double_quotes)
 			in_single_quotes = !in_single_quotes;
-		else if (temp->value[0] == D_QUOTE && temp->quotes_status != SQUOTED)
+		else if (temp->value[0] == D_QUOTE && !in_single_quotes)
 			in_double_quotes = !in_double_quotes;
 		if (in_single_quotes)
 			temp->quotes_status = SQUOTED;
@@ -47,9 +48,8 @@ int	expander(t_token *tokens, t_shell *shell)
 	set_quotes_status(temp);
 	while (temp)
 	{
-		// printf("TOKEN[%d]\ntemp->value: %s\ntemp->type: %d\ntemp->quotes:
-		// %d\n", temp->id, temp->value, temp->type, temp->quotes_status);
-		// 	set_quotes_status(temp);
+		// printf("TOKEN[%d]\ntemp->value: %s\ntemp->type: %d\ntemp->quotes: %d\n",
+		// 	temp->id, temp->value, temp->type, temp->quotes_status);
 		if (temp->type == WORD || temp->type == FILENAME)
 		{
 			i = 0;
