633f3f2b4ea64b522f0e62e96e1e8e3511664321
Alisa Tonkopiy <atonkopi@bess-f4r2s7.clusters.42paris.fr>
2024-05-07 16:53:11 +0200

    added unset functionality

--- src/builtins/ft_unset.c
+++ src/builtins/ft_unset.c
@@ -6,14 +6,56 @@
 /*   By: atonkopi <atonkopi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 13:38:16 by atonkopi          #+#    #+#             */
-/*   Updated: 2024/05/07 15:53:59 by atonkopi         ###   ########.fr       */
+/*   Updated: 2024/05/07 16:52:33 by atonkopi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#include "../../includes/minishell.h"
 
-int ft_unset(t_shell *shell)
+int	var_exists(t_env *env_head, char *var_name)
 {
-	(void)shell;
+	t_env	*env;
+
+	env = env_head;
+	while (env)
+	{
+		if (ft_strcmp(env->var_name, var_name) == 0)
+			return (1);
+		env = env->next;
+	}
+	return (0);
+}
+
+void	delete_env_var(t_env *env_head, char *var_name)
+{
+	t_env	*env;
+	t_env	*prev;
+
+	env = env_head;
+	prev = NULL;
+	while (env)
+	{
+		if (ft_strcmp(env->var_name, var_name) == 0)
+		{
+			if (prev)
+				prev->next = env->next;
+			else
+				env_head = env->next;
+			free(env->var_name);
+			free(env->value);
+			free(env);
+			return ;
+		}
+		prev = env;
+		env = env->next;
+	}
+}
+
+int	ft_unset(t_shell *shell)
+{
+	if (shell->commands->cmd_name[1] == NULL)
+		return (EXIT_FAILURE);
+	if (var_exists(shell->env_head, shell->commands->cmd_name[1]))
+		delete_env_var(shell->env_head, shell->commands->cmd_name[1]);
 	return (EXIT_SUCCESS);
 }
