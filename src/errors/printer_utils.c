d7d352b069d265d6ca3c410e52245b590f8de13e
Julie Labonde <jlabonde@bess-f4r5s11.clusters.42paris.fr>
2024-05-16 12:48:02 +0200

    changed the '

--- src/errors/printer_utils.c
+++ src/errors/printer_utils.c
@@ -6,7 +6,7 @@
 /*   By: jlabonde <jlabonde@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 12:32:04 by jlabonde          #+#    #+#             */
-/*   Updated: 2024/05/14 16:35:39 by jlabonde         ###   ########.fr       */
+/*   Updated: 2024/05/16 12:38:42 by jlabonde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int	syntax_error_in_token(char *token, t_shell *shell)
 {
 	shell->exit_status = 2;
-	ft_putstr_fd("minishell: syntax error near unexpected token '", STDERR_FILENO);
+	ft_putstr_fd("minishell: syntax error near unexpected token `", STDERR_FILENO);
 	ft_putstr_fd(token, 2);
 	ft_putstr_fd("'\n", 2);
 	return (1);
