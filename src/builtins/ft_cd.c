e6b3edc19b11c78c8b8b3587e6f74a511c7ad086
Julie Labonde <jlabonde@bess-f4r1s5.clusters.42paris.fr>
2024-05-29 13:56:50 +0200

    norminette OK

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,42 +6,27 @@
 /*   By: jlabonde <jlabonde@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 12:44:36 by jlabonde          #+#    #+#             */
-/*   Updated: 2024/05/29 09:42:59 by jlabonde         ###   ########.fr       */
+/*   Updated: 2024/05/29 13:55:03 by jlabonde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	update_pwd(t_shell *shell)
+void	update_pwd(t_shell *shell, char *command)
 {
 	char	cwd[4096];
 	char	*old_dir;
 
 	if (getcwd(cwd, sizeof(cwd)) == NULL)
 	{
-		perror("cd: error retrieving current directory: getcwd: cannot access parent directories");
+		ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
+		perror(command);
 		return ;
 	}
 	old_dir = ft_getenv(shell->env_list, "PWD");
 	ft_setenv(shell->env_list, "OLDPWD", old_dir);
 	free(old_dir);
 	ft_setenv(shell->env_list, "PWD", cwd);
-}
-
-int	free_and_return(char *curr_dir, char *old_dir, int ret)
-{
-	if (curr_dir)
-		free(curr_dir);
-	if (old_dir)
-		free(old_dir);
-	return (ret);
-}
-
-int	handle_chdir_error(char *curr_dir, char *old_dir)
-{
-	ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
-	perror(old_dir);
-	return (free_and_return(curr_dir, old_dir, 1));
 }
 
 int	cd_minus(t_shell *shell, int option)
@@ -88,7 +73,7 @@ int	check_for_arguments(t_command *commands, t_shell *shell)
 			perror(commands->cmd_name[1]);
 			return (1);
 		}
-		update_pwd(shell);
+		update_pwd(shell, commands->cmd_name[1]);
 	}
 	return (0);
 }
@@ -116,7 +101,7 @@ int	ft_cd(t_command *commands, t_shell *shell)
 			free(value);
 			return (1);
 		}
-		update_pwd(shell);
+		update_pwd(shell, commands->cmd_name[0]);
 		free(value);
 	}
 	return (check_for_arguments(commands, shell));
