bd6008d4628973ac5cc02eb1e7d37a8f17a428e4
Alice Tonkopiy <adtonkopiy@gmail.com>
2024-05-30 09:45:54 +0200

    changes in cd buildin check and update_pwd funct so that program doesn't crash when PWD/OLDPWD is unset

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -14,19 +14,20 @@
 
 void	update_pwd(t_shell *shell, char *command)
 {
-	char	cwd[4096];
-	char	*old_dir;
+	char	*cwd;
 
-	if (getcwd(cwd, sizeof(cwd)) == NULL)
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
 	{
 		ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
 		perror(command);
 		return ;
 	}
-	old_dir = ft_getenv(shell->env_list, "PWD");
-	ft_setenv(shell->env_list, "OLDPWD", old_dir);
-	free(old_dir);
+	ft_setenv(shell->env_list, "OLDPWD", cwd);
+	free(cwd);
+	cwd = getcwd(NULL, 0);
 	ft_setenv(shell->env_list, "PWD", cwd);
+	free(cwd);
 }
 
 int	cd_minus(t_shell *shell, int option)
@@ -36,17 +37,17 @@ int	cd_minus(t_shell *shell, int option)
 
 	curr_dir = ft_getenv(shell->env_list, "OLDPWD");
 	old_dir = ft_getenv(shell->env_list, "PWD");
-	if (!curr_dir)
-	{
-		write_error("cd", "OLDPWD not set", NULL);
-		return (free_and_return(curr_dir, old_dir, 1));
-	}
-	else if (!old_dir)
-	{
-		write_error("cd", "PWD not set", NULL);
-		return (free_and_return(curr_dir, old_dir, 1));
-	}
-	if (curr_dir && chdir(curr_dir) == -1)
+	// if (!curr_dir)
+	// {
+	// 	write_error("cd", "OLDPWD not set", NULL);
+	// 	return (free_and_return(curr_dir, old_dir, 1));
+	// }
+	// else if (!old_dir)
+	// {
+	// 	write_error("cd", "PWD not set", NULL);
+	// 	return (free_and_return(curr_dir, old_dir, 1));
+	// }
+	if (chdir(old_dir) == -1)
 		return (handle_chdir_error(curr_dir, old_dir));
 	if (option == 1)
 	{
@@ -83,10 +84,10 @@ int	ft_cd(t_command *commands, t_shell *shell)
 	char	*value;
 
 	if (commands->cmd_name[0] && commands->cmd_name[1] && commands->cmd_name[2])
-	{
-		write_error("cd", "too many arguments", NULL);
-		return (1);
-	}
+		return (write_error("cd", "too many arguments", NULL), 1);
+	if (commands->cmd_name[1] && ft_strncmp(commands->cmd_name[1],
+			"---", 3) == 0)
+		return (write_error("cd", "invalid option", "--"), 1);
 	if (!commands->cmd_name[1])
 	{
 		value = ft_getenv(shell->env_list, "HOME");
